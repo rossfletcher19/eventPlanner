6369ec219522320326ab197706a85122a530b0a0
Ross Fletcher <rtbfletch@outlook.com>
2018-01-05 08:53:40 -0800

    have passing instantiate test

--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -8,7 +8,7 @@ public class EventTest {
     @Test
     public void Event_instantiatesCorrect() throws Exception {
         Event testEvent = new Event("Main Course", "Alcohol", "DJ", 40);
-        assertEquals(false, testEvent instanceof Event);
+        assertEquals(true, testEvent instanceof Event);
     }
 
 }
