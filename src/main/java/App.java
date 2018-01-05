ee562a877a03fb4659c73c4df59768ef0583d184
Ross Fletcher <rtbfletch@outlook.com>
2018-01-05 13:37:48 -0800

    add println for entertainment, guests, and newEvent object

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -24,13 +24,40 @@ public class App {
                     break;
                 }
 
-                System.out.println("Great! We've food taken care of but everyone we undoubtedly need something to drink. \nWe have 5 drink services we provide: \n\n[1] Non Alcoholic Beverage Service \n[2] Beer Service \n[3] Full Bar Service \n[4] Beer and Non Alcoholic Beverage \n[5] Full Bar and Non Alcoholic Beverage  \n\n Please enter the corresponding beverage service item # to select your beverage service.");
+                System.out.println("Great! We've food taken care of but everyone we undoubtedly need something to drink. \nWe have 5 drink services we provide: \n\n[1] Non Alcoholic Beverage Service $5/person \n[2] Beer Service $8/person \n[3] Full Bar Service $12/person \n[4] Beer and Non Alcoholic Beverage $12/person \n[5] Full Bar and Non Alcoholic Beverage $17/person  \n\n Please enter the corresponding beverage service item # to select your beverage service.");
                 String bevChoice = bufferedReader.readLine();
 
                 if (!(bevChoice.equals("1") || bevChoice.equals("2") || bevChoice.equals("3") || bevChoice.equals("4") || bevChoice.equals("5"))) {
                     System.out.println("Sorry, was there an input error? Please make sure to select one of our # options.");
                     break;
                 }
+
+                System.out.println("Alright, now that we have food and drinks covered, would you like us to provide any entertainment services? \nWe have 3 services we can provide: \n\n[1] Live DJ - $1000/total \n[2] Live Band - $2500/total \n[3] Comedy Show - $1500/total \n[4] None");
+                String entChoice = bufferedReader.readLine();
+
+                if (!(entChoice.equals("1") || entChoice.equals("2") || entChoice.equals("3") || entChoice.equals("4"))) {
+                    System.out.println("Sorry, was there an input error? Please make sure to select one of our # options.");
+                    break;
+                }
+
+                System.out.println("Now how many guests do you think will be attending?");
+                String NumOfGuests = bufferedReader.readLine();
+                Integer intNumOfGuests = Integer.parseInt(NumOfGuests);
+
+                Event newEvent = new Event(foodServiceChoice, bevChoice, entChoice, intNumOfGuests);
+
+                String orderFood = newEvent.getFood();
+                String orderBev = newEvent.getBeverages();
+                String orderEnt = newEvent.getEntertainment();
+                Integer orderGuests = newEvent.getGuests();
+
+                System.out.println(String.format("------------------------------ \nSo far your order is: \nFood: %s \nBeverage: %s \nEntertainment: %s \nFor %d guests. \n------------------------------", orderFood, orderBev, orderEnt, orderGuests));
+
+                System.out.println("You can now calculate the entire cost of your order if you are happy with it, or you can choose to start over. \n1 for Calculate. \n2 for Start Over.");
+                String calcOrStartOverStep = bufferedReader.readLine();
+
+
+
 
             } catch (IOException e) {
                 e.printStackTrace();
