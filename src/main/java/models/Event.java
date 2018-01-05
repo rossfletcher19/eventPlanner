63badc9c4d14bce9172648583ed1f812e4864ce3
Ross Fletcher <rtbfletch@outlook.com>
2018-01-05 11:11:57 -0800

    add calc cost method

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -40,7 +40,41 @@ public class Event {
         Integer mealTotal = 0;
         Integer bevTotal = 0;
         Integer entTotal = 0;
+        if (food.equals("1")) {
+            mealTotal += (15 * guests);
+        } else if (food.equals("2")) {
+            mealTotal += (8 * guests);
+        } else if (food.equals("3")) {
+            mealTotal += (18 * guests);
+        } else if (food.equals("4")){
+            mealTotal += (22 * guests);
+        } else {
+            System.out.println("There has been an input error. Please enter a valid meal #.");
+        }
 
-        return Integer.valueOf(1500);
+        if (beverages.equals("1")) {
+            bevTotal += (5 * guests);
+        } else if (beverages.equals("2")) {
+            bevTotal += (8 * guests);
+        } else if (beverages.equals("3")) {
+            bevTotal += (12 * guests);
+        } else {
+            System.out.println("There has been an input error. Please enter a valid beverage #.");
+        }
+
+        if (entertainment.equals("1")) {
+            entTotal += 1000;
+        } else if (entertainment.equals("2")) {
+            entTotal += 2500;
+        } else if (entertainment.equals("3")) {
+            entTotal += 1500;
+        } else if (entertainment.equals("none")) {
+            entTotal += 0;
+        } else {
+            System.out.println("There has been an input error. Please enter a valid entertainment #.");
+        }
+
+        int total = mealTotal + bevTotal + entTotal;
+        return total;
     }
 }
